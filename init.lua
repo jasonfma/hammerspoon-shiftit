99c72c305b59072411c89bf976d9cca115e9df32
Jason Ma <7075070+jasonfma@users.noreply.github.com>
2021-07-31 13:32:04 -0700

    Add ratios and stuff

--- init.lua
+++ init.lua
@@ -14,36 +14,40 @@ obj.author = "Peter Klijn"
 obj.homepage = "https://github.com/peterklijn/hammerspoon-shiftit"
 obj.license = ""
 
-obj.mash = { 'ctrl', 'alt', 'cmd' }
+obj.mash = { 'ctrl', 'shift' }
 obj.mapping = {
-  left = { obj.mash, 'left' },
-  right = { obj.mash, 'right' },
-  up = { obj.mash, 'up' },
-  down = { obj.mash, 'down' },
-  upleft = { obj.mash, '1' },
-  upright = { obj.mash, '2' },
-  botleft = { obj.mash, '3' },
-  botright = { obj.mash, '4' },
-  maximum = { obj.mash, 'm' },
+  left = { obj.mash, 'j' },
+  right = { obj.mash, 'l' },
+  up = { obj.mash, 'i' },
+  down = { obj.mash, ',' },
+  upleft = { obj.mash, 'u' },
+  upright = { obj.mash, 'o' },
+  botleft = { obj.mash, 'm' },
+  botright = { obj.mash, '.' },
+  maximum = { obj.mash, 'k' },
   toggleFullScreen = { obj.mash, 'f' },
-  toggleZoom = { obj.mash, 'z' },
-  center = { obj.mash, 'c' },
+  toggleZoom = { obj.mash, 'a' },
+  move_center = { obj.mash, 'x' },
+  move_left= { obj.mash, 'z' },
+  move_right = { obj.mash, 'c' },
   nextScreen = { obj.mash, 'n' },
   previousScreen = { obj.mash, 'p' },
   resizeOut = { obj.mash, '=' },
   resizeIn = { obj.mash, '-' }
 }
 
+local splitRatio = defaultSplitRatio or 0.60
+
 local units = {
-  right50       = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
-  left50        = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
+  right50       = { x = splitRatio, y = 0.00, w = (1.00 - splitRatio), h = 1.00 },
+  left50        = { x = 0.00, y = 0.00, w = splitRatio, h = 1.00 },
   top50         = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
   bot50         = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
   
-  upleft50      = { x = 0.00, y = 0.00, w = 0.50, h = 0.50 },
-  upright50     = { x = 0.50, y = 0.00, w = 0.50, h = 0.50 },
-  botleft50     = { x = 0.00, y = 0.50, w = 0.50, h = 0.50 },
-  botright50    = { x = 0.50, y = 0.50, w = 0.50, h = 0.50 },
+  upleft50      = { x = 0.00, y = 0.00, w = splitRatio, h = 0.50 },
+  upright50     = { x = splitRatio, y = 0.00, w = 1.00 - splitRatio, h = 0.50 },
+  botleft50     = { x = 0.00, y = 0.50, w = splitRatio, h = 0.50 },
+  botright50    = { x = splitRatio, y = 0.50, w = 1.00 - splitRatio, h = 0.50 },
   
   maximum       = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 },
 }
@@ -107,7 +111,8 @@ function resizeWindowInSteps(increment)
       h = notMinHeight and h - hStep * 2 or h
     end
   end
-  hs.window.focusedWindow():move({x=x, y=y, w=w, h=h}, nil, true, 0)
+  -- hs.window.focusedWindow():move({x=x, y=y, w=w, h=h}, nil, true, 0)
+  hs.window.focusedWindow():move({x=x, y=y, w=w, h=window.h}, nil, true, 0)
 end
 
 function obj:left() move(units.left50, nil, true, 0) end
@@ -123,7 +128,11 @@ function obj:maximum() move(units.maximum, nil, true, 0) end
 
 function obj:toggleFullScreen() hs.window.focusedWindow():toggleFullScreen() end
 function obj:toggleZoom() hs.window.focusedWindow():toggleZoom() end
+
 function obj:center() hs.window.focusedWindow():centerOnScreen(nil, true, 0) end
+function obj:move_left() hs.window.focusedWindow():setTopLeft({x=0, y=0}, nil, true, 0) end
+function obj:move_right() hs.window.focusedWindow():setTopLeft({x=hs.window.focusedWindow():screen():frame().w - hs.window.focusedWindow():frame().w, y=0}, nil, true, 0) end
+
 function obj:nextScreen() hs.window.focusedWindow():moveToScreen(hs.window.focusedWindow():screen():next(),false, true, 0) end
 function obj:previousScreen() hs.window.focusedWindow():moveToScreen(hs.window.focusedWindow():screen():previous(),false, true, 0) end
 
@@ -167,11 +176,13 @@ function obj:bindHotkeys(mapping)
   hs.hotkey.bind(self.mapping.botleft[1], self.mapping.botleft[2], function() self:botleft() end)
   hs.hotkey.bind(self.mapping.botright[1], self.mapping.botright[2], function() self:botright() end)
   hs.hotkey.bind(self.mapping.maximum[1], self.mapping.maximum[2], function() self:maximum() end)
-  hs.hotkey.bind(self.mapping.toggleFullScreen[1], self.mapping.toggleFullScreen[2], function() self:toggleFullScreen() end)
-  hs.hotkey.bind(self.mapping.toggleZoom[1], self.mapping.toggleZoom[2], function() self:toggleZoom() end)
-  hs.hotkey.bind(self.mapping.center[1], self.mapping.center[2], function() self:center() end)
-  hs.hotkey.bind(self.mapping.nextScreen[1], self.mapping.nextScreen[2], function() self:nextScreen() end)
-  hs.hotkey.bind(self.mapping.previousScreen[1], self.mapping.previousScreen[2], function() self:previousScreen() end)
+  --hs.hotkey.bind(self.mapping.toggleFullScreen[1], self.mapping.toggleFullScreen[2], function() self:toggleFullScreen() end)
+  --hs.hotkey.bind(self.mapping.toggleZoom[1], self.mapping.toggleZoom[2], function() self:toggleZoom() end)
+  hs.hotkey.bind(self.mapping.move_center[1], self.mapping.move_center[2], function() self:center() end)
+  hs.hotkey.bind(self.mapping.move_left[1], self.mapping.move_left[2], function() self:move_left() end)
+  hs.hotkey.bind(self.mapping.move_right[1], self.mapping.move_right[2], function() self:move_right() end)
+  --hs.hotkey.bind(self.mapping.nextScreen[1], self.mapping.nextScreen[2], function() self:nextScreen() end)
+  --hs.hotkey.bind(self.mapping.previousScreen[1], self.mapping.previousScreen[2], function() self:previousScreen() end)
   hs.hotkey.bind(self.mapping.resizeOut[1], self.mapping.resizeOut[2], function() self:resizeOut() end)
   hs.hotkey.bind(self.mapping.resizeIn[1], self.mapping.resizeIn[2], function() self:resizeIn() end)
 
